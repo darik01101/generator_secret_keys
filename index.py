1172e46f16e20ecbf101e1ad252a8cfdb890d282
darik <darik.ter@gmail.com>
2024-03-28 18:55:35 +0300

    запись ключей в файл

--- index.py
+++ index.py
@@ -1,9 +1,10 @@
 from random import choice, randint
-set_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ1234567890" 
-amount = int (input())
-for _ in range(amount):
-    secret_key = ""
-    for _ in range(randint(20,30)):
-        random_char = choice(set_chars)
-        secret_key += random_char
-    print(secret_key)
+with open('secret_keys.txt', 'wt') as new_file:
+    set_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890" 
+    amount = int (input())
+    for _ in range(amount):
+        secret_key = ""
+        for _ in range(randint(20,30)):
+            random_char = choice(set_chars)
+            secret_key += random_char
+        print(secret_key, file = new_file)
